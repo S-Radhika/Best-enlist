fb129fabed5dda66cea62b6c6177137150db1686
S-Radhika <84444019+S-Radhika@users.noreply.github.com>
2021-06-13 22:37:55 +0530

    Add files via upload

---
+++ day 2.py
@@ -0,0 +1,50 @@
+# print a value
+
+print("30 days 30 hours challenge")
+print('30 days 30 hours challenge')
+
+#assigning string to variable
+
+Hours="thirty"
+print(Hours)
+
+#indexing using string
+
+Days="Thirty days"
+print(Days[5])
+
+#print the particular character from certain text
+
+c= "I win"
+print(c[4:7])
+
+# print length of string
+
+print(len(c))
+
+#convert string into lower case
+
+print(c.lower())
+
+#concatenation
+
+a="30 days"
+b="30 hours"
+l=a+b
+print(l)
+#adding space during concatenation
+
+e=a+" "+b
+print(e)
+
+t="Thirty days and Thirty hours"
+x=t.casefold()
+print(x)
+x=t.capitalize()
+print(x)
+print(x.find("and"))
+print(x.isalnum())
+print(x.isalpha())
+
+
+
