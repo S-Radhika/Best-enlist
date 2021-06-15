c8e94bffdbbcc207466a657eaf853aa44045d502
S-Radhika <84444019+S-Radhika@users.noreply.github.com>
2021-06-15 22:12:30 +0530

    Add files via upload

---
+++ Day 4.py
@@ -0,0 +1,26 @@
+#exercise 1
+a=b=c=150
+print(a/10)
+print(b*50)
+print(c+60)
+
+#exercise 2 replacing a third character with G
+
+r="hello world"
+m=list(r)
+m[3]="G"
+l="".join(m)
+print(l)
+
+#exercise 3 type conversion
+v=6.458
+r=5
+print(v)
+print(r)
+s=float(r)
+print(s)
+n=int(v)
+print(n)
+
+
+
