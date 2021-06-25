3a880d061c46228eec8a7e6160c9f5abc9a35442
S-Radhika <84444019+S-Radhika@users.noreply.github.com>
2021-06-25 07:54:15 +0530

    Add files via upload

---
+++ day 12.py
@@ -0,0 +1,10 @@
+file1=open("30 days 30 hour operations.txt","a")
+file1.write("Radhika\n") 
+file1=open("30 days 30 hours operations.txt","r")
+print(file1.readlines())
+file1.close()
+
+#output
+'''
+I have completed 10 days successfullyRadhika
+'''
